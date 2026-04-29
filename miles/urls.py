d7a5c23f0cc67e135931de497118fe3a53df0200
anjeelika <azzahraanjelikaborselano@gmail.com>
2026-04-29 19:21:30 +0700

    startapp miles & claim miles page

---
+++ miles/urls.py
@@ -0,0 +1,12 @@
+# miles/urls.py
+
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'miles'
+
+urlpatterns = [
+    path('klaim/', TemplateView.as_view(template_name='miles/claim_miles.html'), name='klaim_list'),
+    path('klaim/baru/', TemplateView.as_view(template_name='miles/claim_miles.html'), name='klaim_create'),
+    path('klaim/<int:pk>/hapus/', TemplateView.as_view(template_name='miles/claim_miles.html'), name='klaim_delete'),
+]
