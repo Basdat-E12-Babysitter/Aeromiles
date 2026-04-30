b4327bbbc8716976f5adf80f7f722cf7ba5372dc
AnyaAleena <anya.aleenaa@gmail.com>
2026-04-30 11:56:26 +0700

    fix typo

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path("login/", TemplateView.as_view(template_name="login.html"), name="login"),
     path("registrasi/", TemplateView.as_view(template_name="registrasi.html"), name="register"),
-    path("profile/", TemplateView.as_view(template_name="profile.html", name="profile")),
+    path("profile/", TemplateView.as_view(template_name="profile.html"), name="profile"),
 ]
