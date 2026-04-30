6b9953c1c300264310668dad564db4c4dc2837dd
anjeelika <azzahraanjelikaborselano@gmail.com>
2026-04-30 14:43:02 +0700

    management claim missing miles STAFF (kelola klaim page)

--- miles/urls.py
+++ miles/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('klaim/', TemplateView.as_view(template_name='miles/claim_miles.html'), name='klaim_list'),
     path('klaim/baru/', TemplateView.as_view(template_name='miles/claim_miles.html'), name='klaim_create'),
     path('klaim/<int:pk>/hapus/', TemplateView.as_view(template_name='miles/claim_miles.html'), name='klaim_delete'),
+    path('kelola/', TemplateView.as_view(template_name='miles/kelola_klaim.html'), name='kelola_klaim'),
 ]
