3b07a41ca4b6b3f39f17bce3867def41f90f5ab3
ruzzy13 <uzma.zhafira@gmail.com>
2026-04-30 20:10:56 +0700

    adding templates for module rewards

--- miles/urls.py
+++ miles/urls.py
@@ -12,4 +12,13 @@ urlpatterns = [
     path('kelola/', TemplateView.as_view(template_name='miles/kelola_klaim.html'), name='kelola_klaim'),
     path('transfer/', TemplateView.as_view(template_name='miles/transfer_miles.html'), name='transfer_list'),
     path('transfer/baru/', TemplateView.as_view(template_name='miles/transfer_miles.html'), name='transfer_create'),
+
+    path('redeem/', TemplateView.as_view(template_name='hadiah/redeem_hadiah.html'), name='redeem_list'), 
+    path('package/', TemplateView.as_view(template_name='hadiah/beli_package.html'), name='package_list'),
+    path('tier/', TemplateView.as_view(template_name='hadiah/info_tier.html'), name='tier_info'),
+    path('laporan/', TemplateView.as_view(template_name='hadiah/laporan_transaksi.html'), name='laporan_transaksi'),
+
+    path('redeem/create/', TemplateView.as_view(template_name='hadiah/redeem_hadiah.html'), name='redeem_create'),
+    path('package/beli/', TemplateView.as_view(template_name='hadiah/beli_package.html'), name='package_beli'),
+    path('laporan/hapus/', TemplateView.as_view(template_name='hadiah/laporan_transaksi.html'), name='laporan_hapus'),
 ]
--- rewards/urls.py
+++ rewards/urls.py
@@ -1,8 +1,18 @@
 # rewards/urls.py
-
 from django.urls import path
 from django.views.generic import TemplateView
 
 app_name = 'rewards'
 
-urlpatterns = []
+urlpatterns = [
+    path('redeem/', TemplateView.as_view(template_name='redeem_hadiah.html'), name='redeem_list'), 
+    path('redeem/create/', TemplateView.as_view(template_name='redeem_hadiah.html'), name='redeem_create'),
+
+    path('package/', TemplateView.as_view(template_name='beli_package.html'), name='package_list'),
+    path('package/beli/', TemplateView.as_view(template_name='beli_package.html'), name='package_beli'),
+
+    path('tier/', TemplateView.as_view(template_name='info_tier.html'), name='tier_info'),
+
+    path('laporan/', TemplateView.as_view(template_name='laporan_transaksi.html'), name='laporan_transaksi'),
+    path('laporan/hapus/', TemplateView.as_view(template_name='laporan_transaksi.html'), name='laporan_hapus'),
+]
