9efb098e5ddd28c5ce33b87349f1bbcfd87400cf
newjeasnz <jesiccatandra@gmail.com>
2026-04-30 11:06:56 +0700

    add nav active template tag & update navbar template

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path("login/", TemplateView.as_view(template_name="login.html"), name="login"),
     path("registrasi/", TemplateView.as_view(template_name="registrasi.html"), name="register"),
+    path("profile/", TemplateView.as_view(template_name="profile.html", name="profile")),
 ]
