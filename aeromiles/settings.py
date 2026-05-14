c25e8955d1345651cf28951bf0c856920a02a5a1
kanayradeeva010 <adeevakanayra@gmail.com>
2026-05-14 11:52:01 +0700

    feat: setup postgresql supabase connection

--- aeromiles/settings.py
+++ aeromiles/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,8 +86,12 @@ WSGI_APPLICATION = 'aeromiles.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.wvrzfalrgvepxfgwekuj',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'aws-1-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
